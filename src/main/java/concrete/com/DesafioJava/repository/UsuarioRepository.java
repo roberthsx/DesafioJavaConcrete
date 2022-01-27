9edc0edbb854ef9d88b70aa5c7dc1acfa624e0da
roberthsx <roberthenrrique@gmail.com>
2022-01-27 16:42:52 -0300

    ajustes para postgress

--- src/main/java/concrete/com/DesafioJava/repository/UsuarioRepository.java
+++ src/main/java/concrete/com/DesafioJava/repository/UsuarioRepository.java
@@ -2,10 +2,11 @@ package concrete.com.DesafioJava.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import concrete.com.DesafioJava.model.Usuario;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
 
